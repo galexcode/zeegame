1c858efca69b8108d92aa6f4ec358800a3107626
Max Garrick <maxgarrick3@gmail.com>
2013-09-05 21:52:33 -0700

    Initial commit.  Able to move things around using the mouse

---
+++ Assets/Scripts/camera behavior.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var movementSpeed : float = 5.0f;
+
+function Start () {
+}
+
+function Update () {
+  var velocity : Vector3 = Vector3(Input.GetAxis("Horizontal"), -Input.GetAxis("Mouse ScrollWheel"), Input.GetAxis("Vertical"));
+  
+  if (Input.mousePosition.x <= 0) {
+    velocity.x -= 1.0;
+  } else if (Input.mousePosition.x >= Screen.width) {
+    velocity.x += 1.0;
+  }
+  
+  if (Input.mousePosition.y <= 0) {
+    velocity.z -= 1.0;
+  } else if (Input.mousePosition.y >= Screen.height) {
+    velocity.z += 1.0;
+  }
+  transform.Translate(velocity * movementSpeed * Time.deltaTime, Space.World);
+     
+  if (Input.GetMouseButtonDown(0)) {
+    grabObject();
+  }
+}
+
+
+function grabObject() {
+  var hit: RaycastHit;
+  var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+  if (Physics.Raycast(ray, hit)) {
+    if (hit.collider != null && hit.collider.gameObject.tag == "Movable") {
+      hit.collider.gameObject.SendMessage("grab");
+    }
+  }
+}
---
+++ Assets/Scripts/movable behavior.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+public var grabbed : boolean = false;
+
+function Start () {
+}
+
+function Update () {
+  if (Input.GetMouseButtonUp(0)) { 
+    releaseObject();
+  }
+  
+  if (grabbed) {
+    moveObject();
+  }
+}
+
+
+function releaseObject() {
+  if (grabbed) {
+    grabbed = false;
+    collider.enabled = true;
+  }
+}
+
+function moveObject() {
+  var hit: RaycastHit;
+  var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+  if (Physics.Raycast(ray, hit)) {
+    if (hit.collider != null) {
+      hit.point -= ray.direction;
+      transform.position = hit.point;
+      Debug.Log(hit.point);
+    }
+  }
+}
+
+function grab() {
+  Debug.Log("Grabbed");
+  grabbed = true;
+  collider.enabled = false;
+}
