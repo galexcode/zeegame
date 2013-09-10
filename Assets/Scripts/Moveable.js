7776557de9646c5c32933c226d65ec0d347e80c0
Max Garrick <maxgarrick3@gmail.com>
2013-09-09 20:31:41 -0700

    now movable objects are highlighted when moused over

---
+++ Assets/Scripts/Moveable.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+private var originalColor : Color;
+private var highlighted : boolean = false;
+
+function Start () {
+
+}
+
+function Update () {
+  UnHighlight();
+}
+
+function UnHighlight() {
+  if (highlighted) {
+    renderer.material.SetColor("_Color", originalColor);
+    highlighted = false;
+  }
+}
+
+function Highlight() {
+  if (!highlighted) {
+    highlighted = true;
+    originalColor = renderer.material.color;
+    renderer.material.SetColor("_Color", renderer.material.color + Color(-0.2f, -0.2f, 1f));
+  }
+}
--- Assets/Scripts/TilePlaneMouse.js
+++ Assets/Scripts/TilePlaneMouse.js
@@ -5,6 +5,7 @@
 var currentTileCoordinate : Vector3;
 var selectionCube : Transform;
 private var _tilePlane : TilePlane;
+private var highlightedObject : GameObject;
 
 function Start() {
   _tilePlane = GetComponent(TilePlane);
@@ -18,10 +19,18 @@ function highlight() {
   var hit: RaycastHit;
   var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
   if (Physics.Raycast(ray, hit)) {
-    currentTileCoordinate.x = Mathf.FloorToInt(hit.point.x / _tilePlane.tileSize);
-    currentTileCoordinate.z = Mathf.FloorToInt(hit.point.z / _tilePlane.tileSize);
+    if (hit.collider.gameObject.tag == "Movable") {
+      if (highlightedObject != null) {
+        highlightedObject.SendMessage("UnHighlight");
+      }
+      highlightedObject = hit.collider.gameObject;
+      highlightedObject.SendMessage("Highlight");
+    } else {
+      currentTileCoordinate.x = Mathf.FloorToInt(hit.point.x / _tilePlane.tileSize);
+      currentTileCoordinate.z = Mathf.FloorToInt(hit.point.z / _tilePlane.tileSize);
 
-    selectionCube.transform.position = currentTileCoordinate;
+      selectionCube.transform.position = currentTileCoordinate;
+    }
 
     //if (hit.collider != null && hit.collider.gameObject.tag == "Movable") {
     //  hit.collider.gameObject.SendMessage("grab");
